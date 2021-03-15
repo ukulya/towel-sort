f8371df8e71cde45d536e7cf7a234255355de1cc
ukulya <ukulya150992@gmail.com>
2021-03-15 23:05:34 +0600

    feat: update file

--- src/index.js
+++ src/index.js
@@ -2,5 +2,19 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  var length = matrix.length;
+  var swapped = false;
+  for (var i = 0; i < length; i++){
+    swapped = false;
+    for ( var j = 0; j < length-i-1; j++){
+      if (matrix[j] > matrix[j + 1] ){
+        matrix.swap(j, j + 1);
+        swapped = true;
+      } 
+    }
+ if (!swapped){
+      break;
+    }
+ }
+  return matrix;
 }
